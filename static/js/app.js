d9e67c15abd9de7ac333d0933ba2431d86b90aaa
David Kling <davidkling@Davids-Mac-mini.fios-router.home>
2019-05-16 09:00:48 -0400

    final version of UFO project

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,10 @@
 // from data.js
 let ufosTable = data;
 
-
-// console.log(ufosTable);
-
 // Selecting table body
 let tbody = d3.select('tbody');
-// Populate the table
 
+// Populate the table
 ufosTable.forEach((ufoReport) => {
   let row = tbody.append("tr");
   Object.entries(ufoReport).forEach(([key, value]) => {
@@ -16,26 +13,26 @@ ufosTable.forEach((ufoReport) => {
   });
 });
 
- // Filter the table by Date
- let submit = d3.select("#filter-btn");
- let inputElement = d3.select('#datetime');
- let inputValue = inputElement.property('value')
+ // Variable assignment for 
+ let enterButton  = d3.select("#filter-btn");
+ 
+ // function for displaying filtered date
+ function displayedData(dateFilter) {
+   dateFilter.forEach(function(dateFilter) {
+    let row = tbody.append('tr'); 
+     Object.entries(dateFilter).forEach(function([key,value]) { 
+       let cell = row.append('td');
+       cell.text(value);
+     })})};
 
- console.log(inputValue)
-
-// function filterByDate(ufosTable) {
-//   let filteredData = ufosTable.filter(inputValue === ufosTable.datetime)  
-//   console.log (inputElement);
-//   console.log (inputValue);
-//   console.log (filteredData);
-// }
-
-// updateTable (ufosTable);
-// submit.on("click", function() {
-//   let result = filterByDate (data);
-//   updateTable (result);
-// });
-  
-  
-// Prevent the page from refreshing
-// d3.event.preventDefault();
+// Function for capturing date and click action
+function buttonClick () {
+    d3.event.preventDefault();
+    let inputDate = d3.select('#datetime');
+    let inputValue = inputDate.property('value');
+    let dateFilter = ufosTable.filter(ufoData => ufoData.datetime === inputValue);
+// clear original table out to display filtered data
+    d3.select("tbody").selectAll("tr").remove();
+    displayedData(dateFilter);
+}
+enterButton.on('click', buttonClick);
